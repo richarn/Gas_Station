963c82f489fe6be2c31e356d4fd292942703614a
richar <rnezferreira9@gmail.com>
2018-07-03 21:50:08 -0400

    add the logout option

---
+++ Station/resources/views/layouts/admin.blade.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>FindGas</title>
+
+<link href="{{asset('assets/css/bootstrap.css')}}" rel="stylesheet" type="text/css" media="all">
+<link href="{{asset('assets/css/style.css')}}" rel="stylesheet" type="text/css" media="all" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Location Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script src="{{asset('assets/js/jquery.fancybox.pack.js')}}"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700,800,600,300' rel='stylesheet' type='text/css'>
+<script src="{{asset('assets/js/jquery.min.js')}}"></script>
+<script src="{{asset('assets/js/jquery.easydropdown.js')}}"></script>
+<!-- Mega Menu -->
+<link href="{{asset('assets/css/megamenu.css')}}" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="{{asset('assets/js/megamenu.js')}}"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<!-- Mega Menu -->
+</head>
+<body>
+<!-- banner -->
+	<div class="header">
+		<div class="container" style="text-align: center;">
+			<b style=" 
+  				font-family: Ubuntu;
+  				font-size:300%;
+  				font-style: normal;
+  				color:white;">FINDGAS</b>
+
+			<div class="header-left">
+
+			</div>
+				<div class="clearfix"></div>
+		</div>
+	</div>
+	<div class="header-bottom">
+		<div class="container">
+			<div class="top-nav">
+				<span class="menu"> </span>
+					<ul class="navig megamenu skyblue">
+                                    <li style="text-align: center;">
+                                        <a href="login"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Salir
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                     </ul>               
+						</li>
+
+						<div class="clearfix"></div>
+					</ul>
+					<script>
+					$("span.menu").click(function(){
+						$(".top-nav ul").slideToggle(300, function(){
+						});
+					});
+				</script>
+			</div>
+			<div class="head-right">
+				<ul class="number">
+								
+						
+						<div class="clearfix"> </div>						
+				</ul>
+			</div>
+			<div class="clearfix"> </div>	
+		</div>
+	</div>
+	<div class="banner">
+		<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1mB5G87RWetFr5UN8wOQS5kL1zFA" width="100%" height="500" frameborder="0" style="border:0"></iframe>
+	</div>
+		<div class="banner-info">
+		<div class="container">
+		</div>
+		</div>
+
+	<div class="footer">
+		<div class="container">
+
+			<div class="col-md-2 abo-foo1">
+				<h5>Location</h5>
+				<p>123, street name</p>
+				<p>	landmark,</p>
+				<p>	California 123</p>
+				<p>	Tel: 123-456-7890</p>
+				<p>	Fax. +123-456-7890</p>
+			</div>
+
+				<div class="clearfix"></div>
+		</div>
+	</div>
+</body>
+</html>
